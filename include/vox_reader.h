b8b5b6866135b503bd366526ceba59083fb7e486
Vili Sinervä <vili.m.sinerva@gmail.com>
2020-06-17 23:58:32 +0300

    Refactor model reading into a function

--- include/vox_reader.h
+++ include/vox_reader.h
@@ -13,9 +13,10 @@ using intMatrix_t = std::vector<std::vector<std::vector<int>>>;
 
 void readInt(uint32_t &integer, std::istream &file);
 void readByte(uint8_t &byte, std::istream &file);
-
 uint32_t tagToInt32(char a, char b, char c, char d);
+
 bool isHeaderValid(std::istream &file);
+std::vector<model> readModels(uint32_t &tag, std::istream &file);
 
 intMatrix_t readVoxFile( char* filepath);
 
--- src/vox_reader.cpp
+++ src/vox_reader.cpp
@@ -7,18 +7,18 @@ void readInt(uint32_t &integer, istream &file)
 	file.read(reinterpret_cast<char*>(&integer), sizeof(integer));
 }	
 
-void readByte(uint8_t &byte, std::istream &file)
+void readByte(uint8_t &byte, istream &file)
 {
 	file.read(reinterpret_cast<char*>(&byte), sizeof(byte));
 }	
-
 
 uint32_t tagToInt32(char a, char b, char c, char d)
 {
 	return (a | b<<8 | c<<16 | d<<24);
 }
 
-bool isHeaderValid(std::istream &file)
+
+bool isHeaderValid(istream &file)
 {
 	uint32_t temp_uint32;
 
@@ -50,6 +50,56 @@ bool isHeaderValid(std::istream &file)
 	return true;
 }
 
+vector<model> readModels(uint32_t &tag, istream &file)
+{
+	readInt(tag, file);
+
+	//Variable to assign sequential ids to models
+	int id{0};
+
+	uint32_t sizeTag	{ tagToInt32( 'S', 'I', 'Z', 'E') };
+	vector<model> models{};
+
+	while(tag == sizeTag )
+	{
+		//Skip chunk info after "SIZE" tag (8 bytes)
+		file.seekg(8, ios::cur);
+
+		uint32_t size_x;
+		uint32_t size_y;
+		uint32_t size_z;
+		readInt(size_x, file);
+		readInt(size_y, file);
+		readInt(size_z, file);
+
+		models.push_back( model(id++, size_x, size_y, size_z));
+
+		//Skip "XYZI" tag and its chunk info (12 bytes)
+		file.seekg(12, ios::cur);
+
+		uint32_t nOfModels;
+		uint8_t x{0};
+		uint8_t y{0};
+		uint8_t z{0};
+		uint8_t colByte{0};
+		readInt(nOfModels, file);
+		for( int i{0}; i < nOfModels; ++i)
+		{
+			readByte(x, file);
+			readByte(y, file);
+			readByte(z, file);
+			readByte(colByte, file);
+			models.back().data[x][y][z].colorIndex = colByte;
+		}
+
+		//Read tag for next chunk
+		readInt(tag, file);
+	}
+
+	return models;
+}
+
+
 intMatrix_t readVoxFile( char* filepath)
 {
 	ifstream file;
@@ -58,65 +108,19 @@ intMatrix_t readVoxFile( char* filepath)
 
 	if(file.is_open())
 	{
+		uint32_t tag{0};
+
 		if(isHeaderValid(file) == false)
 			return intMatrix_t{}; 
 
-		uint32_t tag{0};
-		vector<model> models{};
-
-		uint32_t sizeTag	{ tagToInt32( 'S', 'I', 'Z', 'E') };
-		uint32_t transformNodeTag{ tagToInt32( 'n', 'T', 'R', 'N') };
-		uint32_t groupNodeTag{ tagToInt32( 'n', 'G', 'R', 'P') };
-		uint32_t shapeNodeTag{ tagToInt32( 'n', 'S', 'H', 'P') };
-
 		//Skip chunk info after "MAIN" tag (8 bytes) and read next tag
 		file.seekg(8, ios::cur);
-		readInt(tag, file);
-
-		//Variable to assign sequential ids to models
-		int id{0};
 
 		//Read models
-		//Identified by the "SIZE" tag
-		while(tag == sizeTag )
-		{
-			//Skip chunk info after "SIZE" tag (8 bytes)
-			file.seekg(8, ios::cur);
-
-			uint32_t size_x;
-			uint32_t size_y;
-			uint32_t size_z;
-			readInt(size_x, file);
-			readInt(size_y, file);
-			readInt(size_z, file);
-
-			models.push_back( model(id++, size_x, size_y, size_z));
-
-			//Skip "XYZI" tag and its chunk info (12 bytes)
-			file.seekg(12, ios::cur);
-
-			uint32_t nOfModels;
-			uint8_t x{0};
-			uint8_t y{0};
-			uint8_t z{0};
-			uint8_t colByte{0};
-			readInt(nOfModels, file);
-			for( int i{0}; i < nOfModels; ++i)
-			{
-				readByte(x, file);
-				readByte(y, file);
-				readByte(z, file);
-				readByte(colByte, file);
-				models.back().data[x][y][z].colorIndex = colByte;
-			}
-
-			//Read tag for next chunk
-			readInt(tag, file);
-		}
-
+		//Tag will contain first NON-MODEL (SIZE or XYZI) tag!
+		vector<model> models{readModels(tag, file)};
 
 		file.close();
-
 		intMatrix_t tempMatrix{1};
 		return tempMatrix; 
 	}
